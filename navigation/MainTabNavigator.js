a2f8546286f75921ae979b0c2eb247e956650350
Mathias123 <mathiasboampong123@gmail.com>
2020-02-07 00:28:22 +0000

    added the icons

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,8 +5,8 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import SearchScreen from '../screens/SearchScreen';
+import NotificationScreen from '../screens/NotificationScreen';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
@@ -27,8 +27,8 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+          ? `ios-home}`
+          : 'md-home'
       }
     />
   ),
@@ -36,42 +36,59 @@ HomeStack.navigationOptions = {
 
 HomeStack.path = '';
 
-const LinksStack = createStackNavigator(
+const SearchStack = createStackNavigator(
   {
-    Links: LinksScreen,
+    Search: SearchScreen,
   },
   config
 );
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+SearchStack.navigationOptions = {
+  tabBarLabel: 'Search',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
   ),
 };
 
-LinksStack.path = '';
+SearchStack.path = '';
 
-const SettingsStack = createStackNavigator(
+const NotificationStack = createStackNavigator(
   {
-    Settings: SettingsScreen,
+    Notifications: NotificationScreen,
   },
   config
 );
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+NotificationStack.navigationOptions = {
+  tabBarLabel: 'Notifications',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
   ),
 };
 
-SettingsStack.path = '';
+NotificationStack.path = '';
+
+const MailStack = createStackNavigator(
+  {
+    Mail: MailScreen,
+  },
+  config
+);
+
+NotificationStack.navigationOptions = {
+  tabBarLabel: 'Mail',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios-mail' ? 'ios-options' : 'md-mail'} />
+  ),
+};
+
+MailStack.path = '';
 
 const tabNavigator = createBottomTabNavigator({
   HomeStack,
-  LinksStack,
-  SettingsStack,
+  SearchStack,
+  NotificationStack,
+  MailStack
 });
 
 tabNavigator.path = '';
---
+++ screens/MailScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native';
+
+
+
+export default function MailScreen(){
+
+    return(
+        <View>
+            <Text>
+                Send all mails here
+            </Text>
+        </View>
+    )
+}
+
+MailScreen.navigationOptions = {
+  title: 'app.json',
+};
